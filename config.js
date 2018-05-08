a663b03a4bc8f3315b69c354112e33ac39c7ed28
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-05-08 17:06:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -38,14 +38,14 @@ var config = {
     },
   },
   blockchainExplorerOpts: {
-    xsg: {
+    safe: {
       livenet: {
         provider: 'insight',
-        url: 'https://insight.snowgem.org:443',
+        url: 'https://explorer.safecoin.org:443',
       },
       testnet: {
         provider: 'insight',
-        url: 'https://insight.snowgem.org:443',
+        url: 'https://explorer.safecoin.org:443',
         // url: 'http://localhost:3001',
         // Multiple servers (in priority order)
         // url: ['http://a.b.c', 'https://test-insight.bitpay.com:443'],
@@ -55,7 +55,7 @@ var config = {
   pushNotificationsOpts: {
     templatePath: './lib/templates',
     defaultLanguage: 'en',
-    defaultUnit: 'xsg',
+    defaultUnit: 'safe',
     subjectPrefix: '',
     pushServerUrl: 'https://fcm.googleapis.com/fcm',
     authorizationKey: '',
@@ -73,7 +73,7 @@ var config = {
   //  from: 'wallet-service@bitcore.io',
   //  templatePath: './lib/templates',
   //  defaultLanguage: 'en',
-  //  defaultUnit: 'xsg',
+  //  defaultUnit: 'safe',
   //  publicTxUrlTemplate: {
   //    livenet: 'https://insight.bitpay.com/tx/{{txid}}',
   //    testnet: 'https://test-insight.bitpay.com/tx/{{txid}}',
