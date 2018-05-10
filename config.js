fd77c388da05f121d477e4a7cd8904f64f892526
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-05-10 08:38:11 +0300

    fix testnet

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ var config = {
       },
       testnet: {
         provider: 'insight',
-        url: 'https://explorer.safecoin.org:443',
+        url: 'https://testnet.safecoin.org:443',
         // url: 'http://localhost:3001',
         // Multiple servers (in priority order)
         // url: ['http://a.b.c', 'https://test-insight.bitpay.com:443'],
